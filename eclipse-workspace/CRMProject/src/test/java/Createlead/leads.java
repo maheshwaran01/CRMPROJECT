9a1cfeead5fb58a7fdc1dbf280c28c418d89c773
SJB Maheshwaran <maynoormaheshwaran@gmail.com>
2025-01-07 12:50:26 +0530

    initial commit

---
+++ eclipse-workspace/CRMProject/src/test/java/Createlead/leads.java
@@ -0,0 +1,60 @@
+package Createlead;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class leads {
+	
+	public class Credentials02 {
+
+	    WebDriver driver;
+
+	    @BeforeClass
+	    public void setUp() {
+	        // Initialize the ChromeDriver
+	        driver = new ChromeDriver();
+
+	        // Maximize the browser window
+	        driver.manage().window().maximize();
+	    }
+
+	    @Test(priority = 1)
+	    public void openURL() {
+	        // Open the specified URL
+	        driver.get("https://dev.crm.skillcapital.ai/");
+	    }
+
+	    @Test(priority = 2)
+	    public void enterCredentials() {
+	        // Enter Username
+	        WebElement txtbx_username = driver.findElement(By.id("username"));
+	        txtbx_username.sendKeys("admin");
+
+	        // Enter Password
+	        WebElement txtbx_password = driver.findElement(By.name("password"));
+	        txtbx_password.sendKeys("admin@!23");
+	    }
+
+	    @Test(priority = 3)
+	    public void submitLogin() {
+	        // Click on the login button
+	        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Login']"));
+	        loginButton.click();
+	    }
+
+	    @Test(priority = 4)
+	    public void clickSignupLink() {
+	        // Click on the signup link
+	        WebElement signupLink = driver.findElement(By.xpath("//button[text()='login']"));
+	        signupLink.click();
+	    }
+	}
+}
---
+++ eclipse-workspace/CRMProject/src/test/java/Login/credentials.java
@@ -0,0 +1,40 @@
+package Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class credentials {
+	@Test
+	public void creds(){
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://crm.skillcapital.ai/");
+		
+		driver.manage().window().maximize();
+		
+//	
+//	//find username and giving its value
+	WebElement txtbx_username=driver.findElement(By.id("username"));
+			txtbx_username.sendKeys("admin");
+//			
+////find Password and sending key
+		driver.findElement(By.name("password")).sendKeys("admin@!23");
+		
+//		 							(or)
+//		WebElement txt_password=driver.findElement(By.name("password"));
+//		txt_password.sendKeys("admin@!23");
+//		
+		WebElement checkbox = driver.findElement(By.cssSelector("input[type='checkbox']"));
+		 if (!checkbox.isSelected()) { 
+		checkbox.click();
+		 }   
+
+		WebElement LoginButton = driver.findElement(By.xpath("//button[text()='Login']"));
+		LoginButton.click();
+}
+
+}
